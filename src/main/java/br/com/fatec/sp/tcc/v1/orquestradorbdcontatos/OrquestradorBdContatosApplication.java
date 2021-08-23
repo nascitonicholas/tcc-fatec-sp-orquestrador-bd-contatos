1df91f9855b09b12fe4a7f867b782a49a59c011d
nascito_nicholas <nascito.nicholas@gmail.com>
2021-08-23 14:11:13 -0300

    Inicializando projeto.

---
+++ src/main/java/br/com/fatec/sp/tcc/v1/orquestradorbdcontatos/OrquestradorBdContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sp.tcc.v1.orquestradorbdcontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestradorBdContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestradorBdContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/sp/tcc/v1/orquestradorbdcontatos/OrquestradorBdContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sp.tcc.v1.orquestradorbdcontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestradorBdContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
